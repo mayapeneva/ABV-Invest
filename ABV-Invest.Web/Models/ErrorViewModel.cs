6b295c4116f9a90faf437bb0ecaa4cce20b4243a
mayapeneva <maya.peneva@gmail.com>
2018-11-21 01:13:40 +0200

    Created the initial Web project

---
+++ ABV-Invest.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ABV_Invest.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
