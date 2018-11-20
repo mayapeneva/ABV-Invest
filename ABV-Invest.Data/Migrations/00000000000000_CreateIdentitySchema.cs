cd4c432b710df2ef67f86174c64991739a4a9050
mayapeneva <maya.peneva@gmail.com>
2018-11-21 01:22:18 +0200

    Added Data and Models projects

--- ABV-Invest.Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ ABV-Invest.Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace ABV_Invest.Web.Data.Migrations
+namespace ABV_Invest.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
