ef67ab2254cebe041d4c031d27e3dc58796d8a6e
mayapeneva <maya.peneva@gmail.com>
2018-12-20 14:45:27 +0200

    Trying to parse a hand written XML file the normal way

--- ABV-Invest.Web/Areas/Administration/Controllers/UploadsController.cs
+++ ABV-Invest.Web/Areas/Administration/Controllers/UploadsController.cs
@@ -52,96 +52,96 @@ namespace ABV_Invest.Web.Areas.Administration.Controllers
                 if (xmlFile.Length > 0)
                 {
                     //Unfortunatelly this way did not work :(
-                    //using (var stream = new FileStream(filePath, FileMode.Create))
-                    //{
-                    //    await xmlFile.CopyToAsync(stream);
-                    //}
-
-                    //var xmlFileContent = System.IO.File.ReadAllText(filePath);
-                    //var serializer = new XmlSerializer(typeof(New[]), new XmlRootAttribute("WebData"));
-                    //var objPortfolios = (New[])serializer.Deserialize(new StringReader(xmlFileContent));
-
-                    var xmlDoc = new XmlDocument();
-                    xmlDoc.Load(filePath);
-
-                    var portfoliosNodeList = xmlDoc.DocumentElement.SelectNodes("/WebData/New");
-                    var deserializedPortfolios = new List<PortfolioRowBindingModel>();
-                    foreach (XmlNode portfolioNode in portfoliosNodeList)
+                    using (var stream = new FileStream(filePath, FileMode.Create))
                     {
-                        var portfolio = new PortfolioRowBindingModel
-                        {
-                            Client = new Client(),
-                            Instrument = new Instrument(),
-                            AccountData = new AccountData(),
-                            Other = new Other()
-                        };
-                        foreach (XmlElement element in portfolioNode)
-                        {
-                            if (element.Name == "Client")
-                            {
-                                portfolio.Client.CDNNumber = element.SelectSingleNode("CDNumber").InnerText;
-                                portfolio.Client.Name = element.SelectSingleNode("Name").InnerText;
-                            }
-
-                            if (element.Name == "Instrument")
-                            {
-                                portfolio.Instrument.ISIN = element.SelectSingleNode("ISIN").InnerText;
-                                portfolio.Instrument.NewCode = element.SelectSingleNode("NewCode").InnerText;
-                                portfolio.Instrument.Issuer = element.SelectSingleNode("Issuer").InnerText;
-                                portfolio.Instrument.Currency = element.SelectSingleNode("Currency").InnerText;
-
-                                var date = element.SelectSingleNode("MaturityDate").InnerText;
-                                var ifParsed = DateTime.TryParse(date,
-                                    out DateTime maturityDate);
-                                if (!ifParsed)
-                                {
-                                    maturityDate = default(DateTime);
-                                }
-                                portfolio.Instrument.MaturityDate = maturityDate;
-                            }
-
-                            if (element.Name == "AccountData")
-                            {
-                                var number = element.SelectSingleNode("Quantity").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
-                                portfolio.AccountData.Quantity = decimal.Parse(number);
-
-                                number = element.SelectSingleNode("OpenPrice").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
-                                portfolio.AccountData.OpenPrice = decimal.Parse(number);
-
-                                number = element.SelectSingleNode("MarketPrice").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
-                                portfolio.AccountData.MarketPrice = decimal.Parse(number);
-
-                                number = element.SelectSingleNode("MarketValue").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
-                                portfolio.AccountData.MarketValue = decimal.Parse(number);
-
-                                var date = element.SelectSingleNode("MarketDate").InnerText;
-                                var ifParsed = DateTime.TryParse(date,
-                                    out DateTime marketDate);
-                                if (!ifParsed)
-                                {
-                                    marketDate = default(DateTime);
-                                }
-                                portfolio.AccountData.MarketDate = marketDate;
-
-                                number = element.SelectSingleNode("Result").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
-                                portfolio.AccountData.Result = decimal.Parse(number);
-
-                                number = element.SelectSingleNode("ResultBGN").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
-                                portfolio.AccountData.ResultBGN = decimal.Parse(number);
-                            }
-
-                            if (element.Name == "Other")
-                            {
-                                var number = element.SelectSingleNode("YieldPercent").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
-                                portfolio.Other.YieldPercent = decimal.Parse(number);
-
-                                number = element.SelectSingleNode("RelativePart").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
-                                portfolio.Other.RelativePart = decimal.Parse(number);
-                            }
-                        }
-
-                        deserializedPortfolios.Add(portfolio);
+                        await xmlFile.CopyToAsync(stream);
                     }
+
+                    var xmlFileContent = System.IO.File.ReadAllText(filePath);
+                    var serializer = new XmlSerializer(typeof(PortfolioRowBindingModel[]), new XmlRootAttribute("WebData"));
+                    var deserializedPortfolios = (PortfolioRowBindingModel[])serializer.Deserialize(new StringReader(xmlFileContent));
+
+                    //var xmlDoc = new XmlDocument();
+                    //xmlDoc.Load(filePath);
+
+                    //var portfoliosNodeList = xmlDoc.DocumentElement.SelectNodes("/WebData/New");
+                    //var deserializedPortfolios = new List<PortfolioRowBindingModel>();
+                    //foreach (XmlNode portfolioNode in portfoliosNodeList)
+                    //{
+                    //    var portfolio = new PortfolioRowBindingModel
+                    //    {
+                    //        Client = new Client(),
+                    //        Instrument = new Instrument(),
+                    //        AccountData = new AccountData(),
+                    //        Other = new Other()
+                    //    };
+                    //    foreach (XmlElement element in portfolioNode)
+                    //    {
+                    //        if (element.Name == "Client")
+                    //        {
+                    //            portfolio.Client.CDNNumber = element.SelectSingleNode("CDNumber").InnerText;
+                    //            portfolio.Client.Name = element.SelectSingleNode("Name").InnerText;
+                    //        }
+
+                    //        if (element.Name == "Instrument")
+                    //        {
+                    //            portfolio.Instrument.ISIN = element.SelectSingleNode("ISIN").InnerText;
+                    //            portfolio.Instrument.NewCode = element.SelectSingleNode("NewCode").InnerText;
+                    //            portfolio.Instrument.Issuer = element.SelectSingleNode("Issuer").InnerText;
+                    //            portfolio.Instrument.Currency = element.SelectSingleNode("Currency").InnerText;
+
+                    //            var date = element.SelectSingleNode("MaturityDate").InnerText;
+                    //            var ifParsed = DateTime.TryParse(date,
+                    //                out DateTime maturityDate);
+                    //            if (!ifParsed)
+                    //            {
+                    //                maturityDate = default(DateTime);
+                    //            }
+                    //            portfolio.Instrument.MaturityDate = maturityDate;
+                    //        }
+
+                    //        if (element.Name == "AccountData")
+                    //        {
+                    //            var number = element.SelectSingleNode("Quantity").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
+                    //            portfolio.AccountData.Quantity = decimal.Parse(number);
+
+                    //            number = element.SelectSingleNode("OpenPrice").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
+                    //            portfolio.AccountData.OpenPrice = decimal.Parse(number);
+
+                    //            number = element.SelectSingleNode("MarketPrice").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
+                    //            portfolio.AccountData.MarketPrice = decimal.Parse(number);
+
+                    //            number = element.SelectSingleNode("MarketValue").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
+                    //            portfolio.AccountData.MarketValue = decimal.Parse(number);
+
+                    //            var date = element.SelectSingleNode("MarketDate").InnerText;
+                    //            var ifParsed = DateTime.TryParse(date,
+                    //                out DateTime marketDate);
+                    //            if (!ifParsed)
+                    //            {
+                    //                marketDate = default(DateTime);
+                    //            }
+                    //            portfolio.AccountData.MarketDate = marketDate;
+
+                    //            number = element.SelectSingleNode("Result").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
+                    //            portfolio.AccountData.Result = decimal.Parse(number);
+
+                    //            number = element.SelectSingleNode("ResultBGN").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
+                    //            portfolio.AccountData.ResultBGN = decimal.Parse(number);
+                    //        }
+
+                    //        if (element.Name == "Other")
+                    //        {
+                    //            var number = element.SelectSingleNode("YieldPercent").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
+                    //            portfolio.Other.YieldPercent = decimal.Parse(number);
+
+                    //            number = element.SelectSingleNode("RelativePart").InnerText.Replace(".00000).00000.0000", "").Replace(" ", "");
+                    //            portfolio.Other.RelativePart = decimal.Parse(number);
+                    //        }
+                    //    }
+
+                    //    deserializedPortfolios.Add(portfolio);
+                    //}
 
                     var result = this.portfolioService.SeedPortfolios(deserializedPortfolios, model.Date);
                     if (!result.IsCompleted)
