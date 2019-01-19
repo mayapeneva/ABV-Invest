5339b40245ffdacf886420cde57832b1fd76a0f1
mayapeneva <maya.peneva@gmail.com>
2019-01-19 22:45:38 +0200

    Fixed a bug from previous changes

--- ABV-Invest.Web/Extensions/DateValidator.cs
+++ ABV-Invest.Web/Extensions/DateValidator.cs
@@ -6,10 +6,12 @@
 
     public static class DateValidator
     {
+        private const string BeginningDate = "01/01/2016";
+
         public static bool ValidateDate(DateTime dateChosen)
         {
             if (dateChosen.Date > DateTime.UtcNow
-                || dateChosen.Date < DateTime.Parse(Constants.DateTimeShortParseFormat))
+                || dateChosen.Date < DateTime.Parse(BeginningDate))
             {
                 return false;
             }
